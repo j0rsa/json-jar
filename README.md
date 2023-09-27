3559a761bb98801dcff7c509767101656c2ac1aa
red <red.avtovo@gmail.com>
2023-09-27 23:38:50 +0200

    impl

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jsonJar
 api service that stores full or part of the rest json payloads as a file
+
+send a json payload to:
+- `/raw` to log a pretty printed json payload
+- `/csv` to write a json payload to csv file based on the config
+
+## config
+configuration is formed from the following environment variables:
+- `COLUMNS` - the number of columns to save in the csv file
+- `COLUMN_<X>` - the keys chain for the X column. Eg: `COLUMN_1=foo.bar.baz` will save the value of `foo.bar.baz` in the first column
+- `DELIMITER` - the delimiter to use in the csv file. By default `,`
+- `CSV_FILE` - the optional path to the csv file to write to. By default the received line will be only logged
+- `LOG_LEVEL` - the log level. By default `info`
---
+++ src/main.rs
@@ -0,0 +1,140 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde_json::Value;
+use std::io::Write;
+use std::sync::Mutex;
+use log::log;
+
+#[get("/raw")]
+async fn raw(payload: web::Json<Value>) -> impl Responder {
+    match serde_json::to_string_pretty(&payload) {
+        Ok(v) => {
+            log::info!("Payload: {}", v);
+            HttpResponse::Ok().body("OK")
+        }
+        _ => HttpResponse::BadRequest().body(""),
+    }
+}
+
+#[get("/csv")]
+async fn csv(payload: web::Json<Value>, config: web::Data<Mutex<CsvConfig>>) -> impl Responder {
+    let config = config.lock().unwrap();
+    let line = get_csv_line(&Some(payload.0), &config);
+    match line {
+        Err(chaim) => HttpResponse::BadRequest().body(chaim),
+        Ok(l) => {
+            log::info!("Line: {}", l);
+            match config.file {
+                Some(ref file) => {
+                    let mut file = std::fs::OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .open(file)
+                        .unwrap();
+                    file.write_all(l.as_bytes()).unwrap();
+                    file.write_all("\n".as_bytes()).unwrap();
+                }
+                None => {}
+            }
+            HttpResponse::Ok().body("OK")
+        }
+    }
+}
+
+fn get_csv_line(json: &Option<Value>, config: &CsvConfig) -> Result<String, String> {
+    let mut line = Vec::new();
+    for column_chain in &config.columns {
+        let key = get_key(json, column_chain.to_vec());
+        match key {
+            None => return Err(format!("Key not found: {}", column_chain.join("."))),
+            Some(k) => line.push(k),
+        }
+    }
+    Ok(line.join(&config.delimiter))
+}
+
+fn get_key(json: &Option<Value>, key_chain: Vec<String>) -> Option<String> {
+    match json {
+        None => None,
+        Some(j) => {
+            if key_chain.len() == 0 {
+                return if !j.is_array() && !j.is_object() {
+                    if j.is_string() {
+                        Some(j.as_str().unwrap().to_string())
+                    } else {
+                        Some(j.to_string())
+                    }
+                } else {
+                    None
+                };
+            }
+            let key = key_chain[0].clone();
+            let sub_json = j.get(key).map(|s| s.to_owned());
+            get_key(&sub_json, key_chain[1..].to_vec())
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let config = web::Data::new(Mutex::new(CsvConfig::from_env()));
+    HttpServer::new(move || {
+        App::new()
+            .service(raw)
+            .app_data(config.clone())
+            .service(csv)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[derive(Clone)]
+struct CsvConfig {
+    pub columns: Vec<Vec<String>>,
+    pub delimiter: String,
+    #[allow(dead_code)]
+    pub count: usize,
+    pub file: Option<String>,
+}
+
+impl CsvConfig {
+    pub fn from_env() -> Self {
+        let count = std::env::var("COLUMNS").expect("COLUMNS is not set");
+        let count = count.parse::<usize>().expect("COLUMNS is not a number");
+        let delimiter = std::env::var("DELIMITER").unwrap_or(",".to_string());
+        let columns = (0..count)
+            .map(|i| {
+                let key = format!("COLUMN_{}", i);
+                let column_chain =
+                    std::env::var(key.clone()).expect(&format!("{} is not set", key));
+                column_chain
+                    .split(".")
+                    .map(|s| s.to_string())
+                    .collect::<Vec<String>>()
+            })
+            .collect::<Vec<Vec<String>>>();
+        let file = std::env::var("CSV_FILE").ok();
+
+        Self {
+            columns,
+            delimiter,
+            count,
+            file,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn new(
+        columns: Vec<Vec<String>>,
+        delimiter: String,
+        count: usize,
+        file: Option<String>,
+    ) -> Self {
+        Self {
+            columns,
+            delimiter,
+            count,
+            file,
+        }
+    }
+}
